eccbeb087c2fbf807eb46535afcfc675a5e89a95
Sergey-lang <sergei.kuharyonok@yandex.by>
2021-03-20 01:18:51 +0300

    change readme, replaced browser router with hash

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import {App} from './u-1-app/App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './u-1-app/store';
 
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
